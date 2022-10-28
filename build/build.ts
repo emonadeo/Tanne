c303644eef5bbf5bc4f99b59c58ed893a38551e8
Emanuel Pilz <emonadeo@lychee.local>
2022-10-28 10:31:46 +0200

    Added XML compliant color parser

--- build/build.ts
+++ build/build.ts
@@ -1,7 +1,7 @@
 import { renderFile } from "https://deno.land/x/mustache@v0.3.0/mod.ts";
 import { walk } from "https://deno.land/std@0.160.0/fs/walk.ts";
 
-import { colors } from "../colors.ts";
+import { colors, Colors } from "../colors.ts";
 
 const encoder = new TextEncoder();
 const dir = Deno.args[0] || "./";
@@ -10,7 +10,21 @@ for await (const walkEntry of walk(dir, {
 	match: [/\.tpl/i],
 })) {
 	console.log(`Building ${walkEntry.path}`);
+	const model = {
+		...colors,
+		_xml: xmlColors(),
+	};
 	const builtPath = walkEntry.path.replace(/\.tpl/i, "");
-	const builtData = await renderFile(walkEntry.path, colors);
+	const builtData = await renderFile(walkEntry.path, model);
 	await Deno.writeFile(builtPath, encoder.encode(builtData));
+}
+
+function xmlColors() {
+	const res: Colors = {};
+	for (const group in colors) {
+		const entries: [string, string][] = Object.entries(colors[group]);
+		const mappedEntries = entries.map(([k, hex]) => [k, hex.substring(1)]);
+		res[group] = Object.fromEntries(mappedEntries);
+	}
+	return res;
 }
--- colors.ts
+++ colors.ts
@@ -1,21 +1,27 @@
-export const colors = {
+export interface Colors {
+	[group: string]: {
+		[key: string]: string;
+	};
+}
+
+export const colors: Colors = {
 	conifer: {
-		"100": "18201f",
-		"200": "1b2423",
-		"300": "232f2d",
-		"400": "2b3b37",
-		"500": "384d48",
-		"600": "455e59",
-		"700": "67837e",
-		"800": "a8c7c0",
-		"900": "dde5ed",
+		"100": "#18201f",
+		"200": "#1b2423",
+		"300": "#232f2d",
+		"400": "#2b3b37",
+		"500": "#384d48",
+		"600": "#455e59",
+		"700": "#67837e",
+		"800": "#a8c7c0",
+		"900": "#dde5ed",
 	},
 	floral: {
-		red: "f37653",
-		orange: "ffc757",
-		yellow: "ffff57",
-		green: "a7e372",
-		blue: "79caf6",
-		lilac: "d488e7",
+		red: "#f37653",
+		orange: "#ffc757",
+		yellow: "#ffff57",
+		green: "#a7e372",
+		blue: "#79caf6",
+		lilac: "#d488e7",
 	},
 };
